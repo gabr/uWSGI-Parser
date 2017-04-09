c7dca5fe82bdbfe5f06d27080a151fe03978162e
Arkadiusz Gabrys <arkadiuszgabrys@gmail.com>
2017-04-09 23:49:27 +0200

    resolving log entry type

--- uwsgiparser/tests/test_uwsgi.py
+++ uwsgiparser/tests/test_uwsgi.py
@@ -5,17 +5,16 @@ from uwsgiparser.uwsgi import getNextLogEntry, UwsgiLogEntry
 class TestUwsgi(unittest.TestCase):
     def setUp(self):
         self.TEST_LOGS = {
-            "DEBUG":  """[DEBUG] [base] Configuring Raven for host:
-                         <raven.conf.remote.RemoteConfig object at
-                         0x7fdeb1e10b38>""",
-            "INFO_LOG": """[INFO] [base] Raven is not configured (logging is
-                           disabled). Please see the documentation for more
-                           information.""",
-            "ENTRY_LOG": """[pid: 16992|app: 0|req: 1/1] 127.0.0.1 () {44 vars
-                            in 833 bytes} [Mon Nov 21 17:50:21 2016] GET /admin
-                            => generated 23614 bytes in 993 msecs
-                            (HTTP/1.1 404) 3 headers in 94 bytes
-                            (1 switches on core 0)""" 
+            "DEBUG": """[DEBUG] [base] Configuring Raven for host:
+                        <raven.conf.remote.RemoteConfig object at
+                        0x7fdeb1e10b38>""",
+            "INFO":  """[INFO] [base] Raven is not configured (logging is
+                        disabled). Please see the documentation for more
+                        information.""",
+            "ENTRY": """[pid: 16992|app: 0|req: 1/1] 127.0.0.1 () {44 vars
+                        in 833 bytes} [Mon Nov 21 17:50:21 2016] GET /admin
+                        => generated 23614 bytes in 993 msecs (HTTP/1.1 404)
+                        3 headers in 94 bytes (1 switches on core 0)"""
         }
 
         self.LOG_FILE = open('data/logfile.log', 'r')
@@ -27,4 +26,9 @@ class TestUwsgi(unittest.TestCase):
         for k in self.TEST_LOGS.keys():
             line = self.TEST_LOGS[k]
             self.assertEqual(line, UwsgiLogEntry(line).line)
+
+    def test_has_correct_entry_type(self):
+        for k in self.TEST_LOGS.keys():
+            line = self.TEST_LOGS[k]
+            self.assertEqual(k, UwsgiLogEntry(line).type)
 
--- uwsgiparser/uwsgi.py
+++ uwsgiparser/uwsgi.py
@@ -1,3 +1,4 @@
+import re
 
 
 def getNextLogEntry(logFile):
@@ -9,7 +10,28 @@ def getNextLogEntry(logFile):
 
 
 class UwsgiLogEntry():
-    def __init__(self, line):
-        self.line = line;
+    def __parse(self, line):
+        p = re.compile("^\[(.+?)\]", re.IGNORECASE)
+        m = p.match(line)
 
+        if m is None:
+            self.type = "UNKNOWN"
+        else:
+            t = m.groups()[0].upper()
+            if t in self.ENTRIES:
+                self.type = t
+            else:
+                self.type = "ENTRY"
+
+    def __init__(self, line):
+        self.line = line
+        self.ENTRIES = [
+            "UNKNOWN",
+            "INFO",
+            "ERROR",
+            "DEBUG",
+            "ENTRY"
+        ]
+
+        self.__parse(line)
 
