50cc6094ff4f806056abc9ebb3edb5596facb43d
Arkadiusz Gabrys <arkadiuszgabrys@gmail.com>
2017-04-10 23:00:40 +0200

    parsing time arguments and checking file existence

--- uwsgiparser/parser.py
+++ uwsgiparser/parser.py
@@ -1,5 +1,15 @@
 import argparse
+import os.path
+from datetime import datetime
 from uwsgi import getNextLogEntry, UwsgiLogEntry
+
+def parseTime(timeString):
+    try:
+        startTime = datetime.strptime(timeString, "%d-%m-%Y_%H-%M-%S")
+        return startTime
+    except Exception:
+        print("ERROR: Given date time format is invalid: %s" % (timeString))
+        return None
 
 def main():
     """
@@ -13,9 +23,22 @@ def main():
     parser.add_argument("--to", dest="endTime", help='end time, e.g 20-11-2016_11-23-11')
 
     args = parser.parse_args()
-    print args.logPath
-    print args.startTime
-    print args.endTime
+
+    if not os.path.isfile(args.logPath):
+        print("ERROR: Given file not found: %s" % (args.logPath))
+        return
+
+    startTime = None
+    if args.startTime is not None:
+        startTime = parseTime(args.startTime)
+        if startTime is None:
+            return
+
+    endTime = None
+    if args.endTime is not None:
+        endTime = parseTime(args.endTime)
+        if endTime is None:
+            return
 
 
 if __name__ == "__main__":
