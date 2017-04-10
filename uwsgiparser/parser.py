4deba0884f540f6a4ec9374c33dbd46bf095cc9b
Arkadiusz Gabrys <arkadiuszgabrys@gmail.com>
2017-04-10 22:47:27 +0200

    parsing arguments

--- uwsgiparser/parser.py
+++ uwsgiparser/parser.py
@@ -1,11 +1,21 @@
-
+import argparse
+from uwsgi import getNextLogEntry, UwsgiLogEntry
 
 def main():
     """
     Main parser function.
     Parses gien log file and returns statistics.
     """
-    print("Hello World")
+    parser = argparse.ArgumentParser(
+        description='Calculates statistics from uWSGI logs')
+    parser.add_argument('logPath', help='path to uWSGI log file')
+    parser.add_argument("--from", dest="startTime", help='start time, e.g 20-11-2016_11-23-11')
+    parser.add_argument("--to", dest="endTime", help='end time, e.g 20-11-2016_11-23-11')
+
+    args = parser.parse_args()
+    print args.logPath
+    print args.startTime
+    print args.endTime
 
 
 if __name__ == "__main__":
